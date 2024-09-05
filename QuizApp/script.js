09d9acaded068ee6d25437ec6e35f41c8d394c25
GorkemKurtkaya <gorkem.kurtkaya@yahoo.com>
2024-09-05 15:49:08 +0300

    .

--- QuizApp/script.js
+++ QuizApp/script.js
@@ -1,42 +1,66 @@
-// OOP: Nesne Tabanlı Programlama
 
-function Soru(soruMetni, cevapSecenekleri, dogruCevap) {
-    this.soruMetni = soruMetni;
-    this.cevapSecenekleri = cevapSecenekleri;
-    this.dogruCevap = dogruCevap;
-}
-
-Soru.prototype.cevabiKontrolEt = function(cevap) {
-    return cevap === this.dogruCevap
-}
-
-let soru1 = new Soru("Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c");
-let soru2 = new Soru("Hangisi .net paket yönetim uygulasıdır?", { a: "Node.js", b: "Nuget", c: "Npm" }, "b");
-
-let sorular = [
-    new Soru("1-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
-    new Soru("2-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
-    new Soru("3-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
-    new Soru("4-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c")
-];
-
-function Quiz(sorular) {
-    this.sorular = sorular;
-    this.soruIndex = 0;
-}
-
-Quiz.prototype.soruGetir = function() {
-    return this.sorular[this.soruIndex];
-}
 
 const quiz = new Quiz(sorular);
+const ui=new UI();
 
-document.querySelector(".btn-start").addEventListener("click", function() {
-    if (quiz.sorular.length != quiz.soruIndex) {
-        document.querySelector(".quiz-box").classList.add("active");
-        console.log(quiz.soruGetir());
+ui.btn_start.addEventListener("click", function () {
+    ui.quiz_box.classList.add("active");
+    ui.soruGoster(quiz.soruGetir());
+    ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
+    ui.btn_next.classList.remove("show");
+})
+
+ui.btn_next.addEventListener("click", function () {
+    if (quiz.sorular.length != quiz.soruIndex + 1) {
         quiz.soruIndex += 1;
+        ui.soruGoster(quiz.soruGetir());
+        ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
+        ui.btn_next.classList.remove("show");
     } else {
-        console.log("quiz bitti");
+        ui.quiz_box.classList.remove("active");
+        ui.score_box.classList.add("active");
+        ui.skoruGoster(quiz.sorular.length, quiz.dogruCevapSayisi);
     }
 })
+
+// const option_list = document.querySelector(".option_list");
+// const correctIcon = '<div class="icon"><i class="fas fa-check"></i></div>';
+// const incorrectIcon = '<div class="icon"><i class="fas fa-times"></i></div>'
+
+ui.btn_quit.addEventListener("click",function(){
+    window.location.reload();
+});
+
+ui.btn_replay.addEventListener("click",function(){
+    quiz.soruIndex=0;
+    quiz.dogruCevapSayisi=0;
+    ui.btn_start.click();
+    ui.score_box.classList.remove("active");
+});
+
+
+
+function optionSelected(option) {
+    let cevap = option.querySelector("span b").textContent;
+    let soru = quiz.soruGetir();
+
+
+    if (soru.cevabiKontrolEt(cevap)) {
+        quiz.dogruCevapSayisi += 1;
+        option.classList.add("correct");
+        option.insertAdjacentHTML("beforeend", ui.correctIcon);
+    } else {
+        option.classList.add("incorrect");
+        option.insertAdjacentHTML("beforeend", ui.incorrectIcon);
+    }
+
+    for (let i = 0; i < ui.option_list.children.length; i++) {
+        ui.option_list.children[i].classList.add("disabled");
+
+    }
+
+    ui.btn_next.classList.add("show");
+
+}
+
+
